465f151b25a4ea835787cd3c5160120b191ffb4a
bokhtiar <bokhtiar@gmail.com>
2023-10-05 08:26:55 +0600

    Dashboard page dynamic with header footer name change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -33,14 +33,14 @@
 
 
                             <div class="card-body">
-                                <h5 class="card-title">Total course <span></span></h5>
+                                <h5 class="card-title">Total Billing <span></span></h5>
 
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                         <i class="bi bi-book-fill"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>42</h6>
+                                        <h6>{{ App\Models\Billing::count(); }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -52,59 +52,20 @@
                     <div class="col-xxl-3 col-md-6">
                         <div class="card info-card sales-card">
                             <div class="card-body">
-                                <h5 class="card-title">Total course registration</span></h5>
+                                <h5 class="card-title">Total Services</span></h5>
 
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                         <i class="bi bi-bar-chart-fill"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>32</h6>
+                                        <h6>{{ App\Models\Service::count(); }}</h6>
                                     </div>
                                 </div>
                             </div>
 
                         </div>
-                    </div><!-- End registration Card -->
-
-                    <!-- today registration -->
-                    <div class="col-xxl-3 col-md-6">
-                        <div class="card info-card revenue-card">
-                            <div class="card-body">
-                                <h5 class="card-title">Today course registration</h5>
-
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-currency-dollar"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>44</h6>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div><!-- End Revenue Card -->
-
-                    <!-- mentors Card -->
-                    <div class="col-xxl-3 col-xl-12">
-
-                        <div class="card info-card customers-card">
-                            <div class="card-body">
-                                <h5 class="card-title">Mentors</h5>
-
-                                <div class="d-flex align-items-center">
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                        <i class="bi bi-people"></i>
-                                    </div>
-                                    <div class="ps-3">
-                                        <h6>53</h6>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div><!-- End mentors Card -->
+                    </div><!-- End registration Card -->                   
                 </div>
             </div><!-- End Left side columns -->
         </div>
--- resources/views/layouts/admin/partial/footer.blade.php
+++ resources/views/layouts/admin/partial/footer.blade.php
@@ -1,8 +1,5 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; <strong><span>Grap Town</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-        Designed by <a href="https://grapmart.com/">GrapView</a>
+        &copy; <strong><span>Inventory</span></strong>. All Rights Reserved
     </div>
 </footer>
