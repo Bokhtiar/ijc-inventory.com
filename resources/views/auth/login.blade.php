a8e8c8480527485e9b509241ea90983f883e812e
bokhtiar <bokhtiar@gmail.com>
2024-01-13 21:56:09 +0600

    login page desing update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Pages / Login - IJC Management</title>
+    <title>Pages / Login - {{ $setting->company_name }}</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -52,8 +52,13 @@
 
                             <div class="d-flex justify-content-center py-4">
                                 <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                    @if ($setting->logo)
+                                    <img src="{{ asset($setting->logo) }}" alt="">
+                                    <span class="d-none d-lg-block">{{ $setting->company_name }}</span>
+                                    @else
                                     <img src="{{ asset('/admin/assets') }}/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">IJC Management</span>
+                                    <span class="d-none d-lg-block">{{ $setting->company_name }}</span>
+                                    @endif
                                 </a>
                             </div><!-- End Logo -->
 
