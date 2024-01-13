0266fd3ca871b976f51dd404a7fa739092286392
bokhtiar <bokhtiar@gmail.com>
2024-01-13 19:13:42 +0600

    401 page desing

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,10 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Unauthorized</title>
 </head>
+
 <body>
-    <h1>401 Unauthorized</h1>
-    <p>Sorry, you are not authorized to access this page.</p>
+    <img src="{{ asset('assets/401.jpg') }}" width="100%" alt="">
+    <a href="@route('dashboard')" class=""
+        style="
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+">Dashboard</a>
+
 </body>
+
 </html>
