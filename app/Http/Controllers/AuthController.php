18166d2b52121ea4d22e0613303a7ff1242cbabd
Bokhtiar Toshar <bokhtiartoshar@Bokhtiars-MacBook-Air.local>
2024-03-17 01:24:52 +0600

    register create successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,6 +69,7 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -76,20 +77,17 @@ class AuthController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
+            'role_id' => $request->role_id,
             'password' => Hash::make($request->password),
         ]);
 
-        $token = Auth::login($user);
         return response()->json([
             'status' => 'success',
             'message' => 'User created successfully',
             'user' => $user,
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer',
-            ]
         ]);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
